d2033025065c2d55eabe41f345b7c332ed4e7079
Chris Drost <drostie@komodo.(none)>
2010-04-23 08:53:21 +0200

    keccak permutation implemented and standards-checked; padding, speed optimization, minimization, jslint and such to come later.

--- keccak.js
+++ keccak.js
@@ -4,7 +4,7 @@
 
 "use strict";
 var keccak = (function () {
-	var L;
+	var state, State, L, permute, zeros, RC, r, keccak_f;
 	L = function (lo, hi) {
 		this.lo = lo ? lo : 0;
 		this.hi = hi ? hi : 0;
@@ -33,7 +33,10 @@ var keccak = (function () {
 				this.lo = this.hi;
 				this.hi = tmp;
 				n -= 32;
-			} 
+			}
+			if (n === 0) {
+				return this;
+			}
 			m = 32 - n;
 			tmp = (this.hi << n) + (this.lo >>> m);
 			this.lo = (this.lo << n) + (this.hi >>> m);
@@ -45,15 +48,22 @@ var keccak = (function () {
 			hex = function (n) {
 				return ("00" + n.toString(16)).slice(-2);
 			};
+			/*
 			o = function (n) {
 				return hex(n & 255) + hex(n >>> 8) + hex(n >>> 16) + hex(n >>> 24);
 			};
 			return function () {
 				return o(this.lo) + o(this.hi);
 			};
+			*/
+			o = function (n) {
+				return hex(n >>> 24) + hex(n >>> 16) + hex(n >>> 8) + hex(n & 255);
+			};
+			return function () {
+				return o(this.hi) + o(this.lo);
+			};
 		}())
 	};
-	var state, permute, zeros, RC, r, keccak_f;
 	zeros = function (k) {
 		var i, z = [];
 		for (i = 0; i < k; i += 1){
@@ -61,56 +71,100 @@ var keccak = (function () {
 		}
 		return z;
 	};
+	State = function (s) {
+		var fn = function (x, y) {
+			return fn._array[(x % 5) + 5 * (y % 5)];
+		};
+		fn._array = s ? s : zeros(25);
+		fn.clone = function () {
+			return new State(fn._array.map(L.clone));
+		};
+		return fn;
+	}
+		
 	permute = [0,10,20,5,15,16,1,11,21,6,7,17,2,12,22,23,8,18,3,13,14,24,9,19,4];
-	state = zeros(25);
-	RC = "0 1,0 8082,z 808A,z yy,0 808B,0 y0001,z y8081,z 8009,0 8A,0 88,0 y8009,0 y000A,0 y808B,z 8B,z 8089,z 8003,z 8002,z 80,0 800A,z y000A,z y8081,z 8080,0 y0001,z y8008"
-		.replace("z","y0000").replace("y","8000").split(",").map(function(i) {
-			var k = i.split(" ");
+	RC = "0,1;0,8082;z,808A;z,yy;0,808B;0,y0001;z,y8081;z,8009;0,8A;0,88;0,y8009;0,y000A;0,y808B;z,8B;z,8089;z,8003;z,8002;z,80;0,800A;z,y000A;z,y8081;z,8080;0,y0001;z,y8008"
+		.replace(/z/g,"80000000").replace(/y/g,"8000").split(";").map(function(str) {
+			var k = str.split(",");
 			return new L(parseInt(k[1], 16), parseInt(k[0], 16));
 	});
 	r = [0,1,62,28,27,36,44,6,55,20,3,10,43,25,39,41,45,15,21,8,18,2,61,56,14];
 	keccak_f = function () {
-		var x, y, i, B, C, D, round;
+		var x, y, i, B, C, D, round, log;
 		for (round = 0; round < 24; round += 1) {
+			log = round < 5;
+			console.log("Round " + round);
+			// THETA STEP
 			C = zeros(5);
-			for (i = 0; i < 25; i += 1) {
-				C[i % 5].xor(state[i]);
-			}
-			D = C.map(L.clone);
 			for (x = 0; x < 5; x += 1) {
-				D[x].circ(1).xor(C[(x+3)%5]);
-			}
-			for (i = 0; i < 25; i += 1) {
-				state[i].xor(D[(i+1)%5]).circ(r[i]);
-			}
-			console.log(1);
-			last = state.slice(0);
-			for (i = 0; i < 25; i += 1) {
-				state[i] = last[permute[i]];
-			};
-			
-			console.log(2);
-			for (y = 0; y < 25; y += 5) {
-				for (x = 0; x < 5; x += 1) {
-					state[y+x].xor(
-						state[y + (x + 1) % 5].not().and(
-							state[y + (x + 2) % 5]
-						)
-					);
+				for (y = 0; y < 5; y += 1) {
+					C[x].xor(state(x,y));
 				}
 			}
-			console.log(3);
-			state[0].xor(RC[round]);
+			// Extra logic needed because L() objects are dynamic.
+			// D[x] = C[x + 1]
+			D = C.map(L.clone);
+			D = D.concat(D.splice(0, 1));
+			// D[x] = C[x - 1] xor rot(C[x+1], 1)
+			for (x = 0; x < 5; x += 1) {
+				D[x].circ(1).xor(C[(x + 4) % 5]);
+			}
+			for (x = 0; x < 5; x += 1) {
+				for (y = 0; y < 5; y += 1) {
+					state(x,y).xor(D[x]);
+				}
+			}
+			if (log) {
+				console.log("after theta\n", state._array.join(" "));
+			}
+			// RHO STEP
+			B = state.clone()
+			for (x = 0; x < 5; x += 1) {
+				for (y = 0; y < 5; y += 1) {
+					state(x,y).circ(r[5*y + x]);
+				}
+			}
+			if (log) {
+				console.log("after rho\n", state._array.join(" "));
+			}
+			// PI STEP
+			last = state._array.slice(0);
+			for (i = 0; i < 25; i += 1) {
+				state._array[permute[i]] = last[i];
+			}
+			if (log) {
+				console.log("after pi\n", state._array.join(" "));
+			}
+			
+			// CHI STEP
+			B = state.clone();
+			for (x = 0; x < 5; x += 1) {
+				for (y = 0; y < 5; y += 1) {
+					state(x, y).xor(B(x + 1, y).not().and(B(x + 2, y)));
+				}
+			}
+			if (log) {
+				console.log("after chi\n", state._array.join(" "));
+			}
+			// IOTA STEP
+			state(0, 0).xor(RC[round]);
+			if (log) {
+				console.log("after iota\n", state._array.join(" "));
+			}
 		}
 	};
 	function attempt(lo, hi) {
-		state = zeros(25);
-		state[0] = new L(lo, hi);
+		state = new State();
+		state(0, 0).xor(new L(lo, hi));
 		keccak_f();
-		console.log(state[0] + " " + state[1]);
+		console.log(state(0, 0) + " " + state(1, 0));
 	}
-	attempt(0x397B5853, 0x0001901C);
-	attempt(0x53587b39, 0x1C900100);
+	attempt(0, 0);
+	console.log(RC.join(" "));
+	//attempt(0x397B5853, 0x0001901C);
+	//attempt(0x53587b39, 0x1C900100);
+	//attempt(0xe4f6b0a6, 0x008090e0);
+	//attempt(0xa6b0f6e4, 0xe0908000);
 }());
 /*
 	return function (m) {
